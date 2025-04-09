f3a173757660e0b7ae45fd481d5240221f68e359
R3ndrex <yacholohchtoli@gmail.com>
2025-04-09 21:46:33 +0300

    Shorten code by using map for each pollutant

--- src/components/DataSection.jsx
+++ src/components/DataSection.jsx
@@ -1,27 +1,19 @@
 import PollutionForecast from "./PollutionForecast";
 export default function DataSection({ data }) {
+    const DailyArray = Object.entries(data.data.forecast.daily);
+    console.log(DailyArray);
     return (
         <main className="flex gap-5 flex-col items-center mb-[5rem]">
             <section>
                 <h1 className="text-2xl">{data.data.city.name}</h1>
                 <p>AQI: {data.data.aqi}</p>
             </section>
-            <PollutionForecast
-                pollutionData={data.data.forecast.daily.o3}
-                pollutionName={"O3"}
-            />
-            <PollutionForecast
-                pollutionData={data.data.forecast.daily.pm10}
-                pollutionName={"PM10"}
-            />
-            <PollutionForecast
-                pollutionData={data.data.forecast.daily.pm25}
-                pollutionName={"PM25"}
-            />
-            <PollutionForecast
-                pollutionData={data.data.forecast.daily.uvi}
-                pollutionName={"UVI"}
-            />
+            {DailyArray.map(([pollutionName, pollutionValue]) => (
+                <PollutionForecast
+                    pollutionData={pollutionValue}
+                    pollutionName={pollutionName}
+                />
+            ))}
         </main>
     );
 }

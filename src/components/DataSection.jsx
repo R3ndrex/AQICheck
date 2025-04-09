00e9b6a9de228b4dd320921b6c3e2d4df95b6d85
R3ndrex <yacholohchtoli@gmail.com>
2025-04-09 20:46:05 +0300

    Make text bigger, align items to center

--- src/components/DataSection.jsx
+++ src/components/DataSection.jsx
@@ -1,62 +1,62 @@
 export default function DataSection({ data }) {
     return (
-        <>
+        <main className="flex gap-5 flex-col items-center">
             <section>
-                <h1>{data.data.city.name}</h1>
+                <h1 className="text-2xl">{data.data.city.name}</h1>
                 <p>AQI: {data.data.aqi}</p>
             </section>
-            <section>
+            <section className="inline-flex gap-5 border-1 ">
                 <h2>o3 Forecast</h2>
                 {data.data.forecast.daily.o3.map((day) => (
-                    <>
+                    <div className="flex flex-col">
                         <h3>Day: {day.day}</h3>
                         <ul>
                             <li>Max: {day.max}</li>
                             <li>Min: {day.min}</li>
                             <li>Average: {day.avg}</li>
                         </ul>
-                    </>
+                    </div>
                 ))}
             </section>
-            <section>
+            <section className="inline-flex gap-5 border-1 ">
                 <h2>pm10 Forecast</h2>
                 {data.data.forecast.daily.pm10.map((day) => (
-                    <>
+                    <div className="flex flex-col">
                         <h3>Day: {day.day}</h3>
                         <ul>
                             <li>Max: {day.max}</li>
                             <li>Min: {day.min}</li>
                             <li>Average: {day.avg}</li>
                         </ul>
-                    </>
+                    </div>
                 ))}
             </section>
-            <section>
+            <section className="inline-flex gap-5 border-1 ">
                 <h2>pm25 Forecast</h2>
                 {data.data.forecast.daily.pm25.map((day) => (
-                    <>
+                    <div className="flex flex-col">
                         <h3>Day: {day.day}</h3>
                         <ul>
                             <li>Max: {day.max}</li>
                             <li>Min: {day.min}</li>
                             <li>Average: {day.avg}</li>
                         </ul>
-                    </>
+                    </div>
                 ))}
             </section>
-            <section>
+            <section className="inline-flex gap-5 border-1 ">
                 <h2>UVI Forecast</h2>
                 {data.data.forecast.daily.uvi.map((day) => (
-                    <>
+                    <div className="flex flex-col">
                         <h3>Day: {day.day}</h3>
                         <ul>
                             <li>Max: {day.max}</li>
                             <li>Min: {day.min}</li>
                             <li>Average: {day.avg}</li>
                         </ul>
-                    </>
+                    </div>
                 ))}
             </section>
-        </>
+        </main>
     );
 }

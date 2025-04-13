25d6f3f545ed087c0b01014ce6925c674444ad9b
R3ndrex <yacholohchtoli@gmail.com>
2025-04-13 19:03:20 +0300

    Set visible false when clicked outside of opened aside

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 export default function Template() {
+    const ref = useRef();
     const [visible, setVisible] = useState(false);
     return (
         <>
-            <Outlet />
             <div
-                className="burger m-5 absolute top-0 right-0 w-[3rem] h-[3rem]"
-                onClick={() => setVisible((prev) => !prev)}
+                onClick={(e) => {
+                    if (ref && !ref.current.contains(e.target) && visible) {
+                        setVisible(false);
+                    }
+                }}
             >
-                <div className="h-[1rem]"></div>
-                <div className="h-[1rem]"></div>
-                <div className="h-[1rem]"></div>
+                <Outlet />
+                <div
+                    className="burger m-5 absolute top-0 right-0 w-[3rem] h-[3rem]"
+                    onClick={() => setVisible((prev) => !prev)}
+                >
+                    <div className="h-[1rem]"></div>
+                    <div className="h-[1rem]"></div>
+                    <div className="h-[1rem]"></div>
+                </div>
             </div>
             <aside
+                ref={ref}
                 className={`absolute text-2xl flex flex-col gap-5 justify-center items-center top-0 right-0 bg-emerald-200 h-[100vh] w-[25%] ${
-                    visible ? "" : "invisible"
+                    visible ? "opening" : "closing"
                 }`}
             >
                 <Link to={"/map"}>View Map</Link>

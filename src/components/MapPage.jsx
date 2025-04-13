9627d1893eb0c6be42a0a67b49c41824e97f8f06
R3ndrex <yacholohchtoli@gmail.com>
2025-04-13 18:37:04 +0300

    Add titles to markers, add h2 for each map

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,19 +1,27 @@
 import Map from "./Map";
+
+const markerTypes = [
+    { marker: "usepa-aqi", title: "Air Quality Index" },
+    { marker: "usepa-pm25", title: "PM25" },
+    { marker: "usepa-10", title: "PM10" },
+    { marker: "usepa-o3", title: "O3" },
+    { marker: "usepa-no2", title: "NO3" },
+    { marker: "usepa-so2", title: "SO2" },
+    { marker: "usepa-co", title: "CO" },
+];
+
 export default function MapPage() {
-    const position = [50.4501, 30.5236];
-    const markerTypes = [
-        "usepa-aqi",
-        "usepa-pm25",
-        "usepa-10",
-        "usepa-o3",
-        "usepa-no2",
-        "usepa-so2",
-        "usepa-co",
-    ];
+    const position = [51.5074, -0.1278];
+
     return (
-        <main className="flex justify-center items-center m-5">
-            {markerTypes.map((markerType) => {
-                return <Map position={position} markerType={markerType} />;
+        <main className="flex flex-col gap-5 justify-center items-center m-5">
+            {markerTypes.map(({ marker, title }) => {
+                return (
+                    <section className="flex flex-col justify-center items-center">
+                        <h2 className="text-2xl capitalize ">{title}</h2>
+                        <Map position={position} markerType={marker} />
+                    </section>
+                );
             })}
         </main>
     );

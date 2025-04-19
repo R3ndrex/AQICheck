4a482784f9dcd5963680e4927db7381a0fb0db04
R3ndrex <yacholohchtoli@gmail.com>
2025-04-19 19:55:53 +0300

    Add flex wrap to wrap on small screens

--- src/components/PollutionForecast.jsx
+++ src/components/PollutionForecast.jsx
@@ -2,7 +2,7 @@ import PollutionChart from "./PollutionChart";
 
 export default function PollutionForecast({ pollutionData, pollutionName }) {
     return (
-        <section className="inline-flex gap-5 border-1 p-5">
+        <section className="inline-flex gap-5 flex-wrap border-1 p-5">
             <div className="flex flex-col items-center">
                 <h2 className="text-2xl capitalize">
                     {pollutionName} Forecast

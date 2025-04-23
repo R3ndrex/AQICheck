a2f85db922c7ae7d9790736c43b2d120530d92a5
R3ndrex <yacholohchtoli@gmail.com>
2025-04-23 16:40:07 +0300

    add transition to taliwind classes to fix showing animation on starting screen, add margin top to first element of aside

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -24,11 +24,13 @@ export default function Template() {
             </div>
             <aside
                 ref={ref}
-                className={`absolute text-2xl flex flex-col gap-5 justify-center items-center top-0 right-0 bg-emerald-200 h-[100vh] w-[25%] ${
-                    visible ? "opening" : "closing"
+                className={`absolute h-[100%] text-2xl flex flex-col gap-5 items-center top-0 right-0 bg-emerald-200 h-[100vh] w-[25%] ease-in-out duration-[1s] transition ${
+                    visible ? "translate-x-0" : "translate-x-full"
                 }`}
             >
-                <Link to={"/map"}>View Map</Link>
+                <Link className="mt-8" to={"/map"}>
+                    View Map
+                </Link>
                 <Link to={""}>View Main Page</Link>
             </aside>
         </>

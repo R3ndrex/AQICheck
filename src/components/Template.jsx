8a08f7b681de69c97b6de9442a8a56566e6fe006
R3ndrex <yacholohchtoli@gmail.com>
2025-04-27 21:07:06 +0300

    add padding 1

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -41,7 +41,7 @@ export default function Template() {
             </div>
             <aside
                 ref={ref}
-                className={`fixed h-[100%] text-2xl flex flex-col gap-5 items-center top-0 right-0 bg-emerald-200 h-[100vh] w-[25%] ease-in-out duration-[1s] transition ${
+                className={`fixed h-[100%] text-2xl flex flex-col gap-5 items-center top-0 right-0 p-1 bg-emerald-200 h-[100vh] w-[25%] ease-in-out duration-[1s] transition ${
                     visible ? "translate-x-0" : "translate-x-full"
                 }`}
             >

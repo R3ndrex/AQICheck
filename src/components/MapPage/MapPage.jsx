aa0e24d47e5f213da58ec62f5114f720590cb746
R3ndrex <yacholohchtoli@gmail.com>
2025-04-27 16:40:36 +0300

    Remove marker types, create only one map

--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -1,35 +1,22 @@
 import { useOutletContext } from "react-router-dom";
 import Map from "./Map";
-const DEFAULT_POSITION = [51.5074, -0.1278];
-
-const markerTypes = [
-    { marker: "usepa-aqi", title: "Air Quality Index" },
-    { marker: "usepa-pm25", title: "PM25" },
-    { marker: "usepa-10", title: "PM10" },
-    { marker: "usepa-o3", title: "O3" },
-    { marker: "usepa-no2", title: "NO3" },
-    { marker: "usepa-so2", title: "SO2" },
-    { marker: "usepa-co", title: "CO" },
-];
+const DEFAULT_POSITION = [51.5074, -0.1278]; // London
 
 export default function MapPage() {
     const context = useOutletContext() || {};
     const { longitude, latitude } = context;
     return (
         <main className="flex flex-col gap-5 justify-center items-center m-5">
-            {markerTypes.map(({ marker, title }) => (
-                <section className="flex flex-col justify-center items-center">
-                    <h2 className="text-2xl capitalize ">{title}</h2>
-                    <Map
-                        position={
-                            longitude && latitude
-                                ? [latitude, longitude]
-                                : DEFAULT_POSITION
-                        }
-                        markerType={marker}
-                    />
-                </section>
-            ))}
+            <section className="flex flex-col justify-center items-center">
+                <h2 className="text-2xl capitalize">Air Quality Index</h2>
+                <Map
+                    position={
+                        longitude && latitude
+                            ? [latitude, longitude]
+                            : DEFAULT_POSITION
+                    }
+                />
+            </section>
         </main>
     );
 }

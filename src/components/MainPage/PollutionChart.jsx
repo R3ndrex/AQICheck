982ef19a1fb381a5a4e3a03bd2f4257cf93479d1
R3ndrex <yacholohchtoli@gmail.com>
2025-04-27 21:23:47 +0300

    Make graph more responsive to size of device

--- src/components/MainPage/PollutionChart.jsx
+++ src/components/MainPage/PollutionChart.jsx
@@ -1,4 +1,11 @@
-import { LineChart, Line, XAxis, CartesianGrid, Tooltip } from "recharts";
+import {
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+} from "recharts";
 function formatDate(date) {
     const newDate = new Date(date);
     return `${String(newDate.getMonth() + 1).padStart(2, "0")}.${String(
@@ -7,24 +14,28 @@ function formatDate(date) {
 }
 export default function PollutionChart({ data, dataKeys }) {
     return (
-        <LineChart height={500} width={500} data={data}>
-            {dataKeys.map((key) => (
-                <Line
-                    type="monotone"
-                    dataKey={key}
-                    stroke={
-                        key === "max"
-                            ? "#ff0000"
-                            : key === "min"
-                            ? "#008000"
-                            : "#ffd900"
-                    }
-                />
-            ))}
+        <div className="graph">
+            <ResponsiveContainer>
+                <LineChart data={data}>
+                    {dataKeys.map((key) => (
+                        <Line
+                            type="monotone"
+                            dataKey={key}
+                            stroke={
+                                key === "max"
+                                    ? "#ff0000"
+                                    : key === "min"
+                                    ? "#008000"
+                                    : "#ffd900"
+                            }
+                        />
+                    ))}
 
-            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey={"day"} tickFormatter={formatDate} />
-            <Tooltip />
-        </LineChart>
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey={"day"} tickFormatter={formatDate} />
+                    <Tooltip />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     );
 }
